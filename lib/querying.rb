f9eddd421ac68857b2205416e9af659e8af64c1f
Hoon Lee <yl.hoony@gmail.com>
2017-12-22 23:14:16 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -6,7 +6,7 @@ def select_books_titles_and_years_in_first_series_order_by_year
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "SELECT name, motto, MAX(motto) FROM characters;"
+  "SELECT name, MAX(motto) FROM characters;"
 end
 
 
