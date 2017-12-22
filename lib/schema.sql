376a2d10803d6199e864e0430703ba0c3794a82d
Hoon Lee <yl.hoony@gmail.com>
2017-12-22 22:42:42 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -17,9 +17,16 @@ CREATE TABLE authors (
 
 CREATE TABLE books (
   id INTEGER PRIMARY KEY,
-  title TEXT, 
+  title TEXT,
   year TEXT,
   series_id INTEGER
-)
+);
 
-
+CREATE TABLE characters (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  species TEXT,
+  motto TEXT,
+  series_id INTEGER,
+  author_id INTEGER
+);
