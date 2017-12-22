bf13233c372e277a2eacb3294987d55f9fb85079
Hoon Lee <yl.hoony@gmail.com>
2017-12-22 23:38:42 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -27,9 +27,12 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title, characters.species FROM series
+  "SELECT series.title FROM series
   INNER JOIN characters
-  ON series.id = characters.series_id"
+  ON series.id = characters.series_id
+  WHERE characters.species = 'human'
+  GROUP BY series.title
+  ORDER BY COUNT(characters.species) DESC LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
