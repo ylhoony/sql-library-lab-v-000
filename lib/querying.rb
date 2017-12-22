12d77bc1aaac37e43f6ed293fe581ce2dfcb22c1
Hoon Lee <yl.hoony@gmail.com>
2017-12-22 23:52:04 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -38,7 +38,7 @@ end
 def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name, COUNT(*)
   FROM characters
-  INNER JOIN character_books ON characters.series_id = character_books.series_id
+  INNER JOIN character_books ON characters.id = character_books.series_id
   INNER JOIN books ON character_books.book_id = books.id
   GROUP BY characters.name;
   "
