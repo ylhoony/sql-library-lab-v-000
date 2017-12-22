5649eb278d1cecc28d4c2d827d09ecc05dcfd3fd
Hoon Lee <yl.hoony@gmail.com>
2017-12-22 23:44:04 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -36,9 +36,10 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name, books.title
+  "SELECT characters.name, COUNT(books.name)
   FROM characters
   INNER JOIN series ON characters.series_id = series.id
   INNER JOIN books ON series.id = books.series_id
+  GROUP BY characters.name
   "
 end
