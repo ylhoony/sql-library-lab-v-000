cb093b27922b5efc5f428e59ba63c040f3607d95
Hoon Lee <yl.hoony@gmail.com>
2017-12-22 23:42:24 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -36,5 +36,8 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT characters.name, COUNT(books.*)
+  FROM characters
+  INNER JOIN series ON characters.series_id = series.id
+  INNER JOIN books ON series.id = books.series_id"
 end
