424d27abda44d64228c9fcf0f180a7aacc3296f5
Hoon Lee <yl.hoony@gmail.com>
2017-12-22 23:26:20 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -19,7 +19,9 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT authors.name, subgenres.name
+  FROM authors
+  INNER JOIN series ON authors.id = series.author_id"
 end
 
 def select_series_title_with_most_human_characters
